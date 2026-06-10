73762adf0e23f5b408ed2f0d638e055822d9e61d
Jona0002 <jona0002@student.monash.edu>
2026-06-11 01:34:34 +0800

    Fix producer code

--- start.sh
+++ start.sh
@@ -4,4 +4,6 @@ sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/setup_mongodb
 
 sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/streaming_app.py
 
-sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/producer.py
+sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/producer_a.py &
+sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/producer_b.py &
+sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/producer_c.py &
