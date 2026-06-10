eba8d1c4aa1a817dda6b9149705002db05e7a06a
Jona0002 <jona0002@student.monash.edu>
2026-06-11 01:42:02 +0800

    Adds start producer code

--- start.sh
+++ start.sh
@@ -3,7 +3,3 @@ sudo docker compose up -d
 sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/setup_mongodb.py
 
 sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/streaming_app.py
-
-sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/producer_a.py &
-sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/producer_b.py &
-sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/producer_c.py &
