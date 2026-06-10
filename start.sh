e0ae2a5cab3210914296a104ff109782015d0528
Jona0002 <jona0002@student.monash.edu>
2026-06-11 00:10:05 +0800

    Fixes container id in start script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 sudo docker compose up -d
 
-sudo docker exec -it fit3182-a3_pyspark_1 python /home/student/src/setup_mongodb.py
+sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/setup_mongodb.py
 
-sudo docker exec -it fit3182-a3_pyspark_1 python /home/student/src/streaming_app.py
+sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/streaming_app.py
 
-sudo docker exec -it fit3182-a3_pyspark_1 python /home/student/src/producer.py
+sudo docker exec -it fit3182-a3-pyspark-1 python /home/student/src/producer.py
